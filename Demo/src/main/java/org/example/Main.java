ff0a55d592237b9e99ecc950664418a338f9891e
aurelie661 <boutaurel59@gmail.com>
2023-08-16 16:25:02 +0200

    Démo connection jdbc-postgreSQL

---
+++ Demo/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*try{
+            Connection conn = DriverManager.getConnection(dbUrl,userName,userPassword);
+            if(conn != null){
+                System.out.println("Connexion OK !");
+            }else{
+                System.out.println("Connexion ECHOUE !");
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }*/
+
+    }
+}
