84e2fb30a233f7b43e94e1aaa0838ad55dfef22f
aurelie661 <boutaurel59@gmail.com>
2023-08-16 21:44:23 +0200

    demo ajout connection

--- Demo/src/main/java/org/example/Demo1.java
+++ Demo/src/main/java/org/example/Demo1.java
@@ -12,15 +12,15 @@ public class Demo1 {
 
         try{
             conn = DatabaseManager.getPostgresSQLConnection();
-            /*System.out.println("Merci de saisir le prénom : ");
+            System.out.println("Merci de saisir le prénom : ");
             String firstName = scanner.nextLine();
             System.out.println("Merci de saisir le nom : ");
-            String lastName = scanner.nextLine();*/
+            String lastName = scanner.nextLine();
 
-            /*//execution de requete sans retour
-            String sql = "INSERT INTO person (firstName,lastName) VALUES ('"+firstName+"','"+lastName+"')";
+            //execution de requete sans retour
+            //String sql = "INSERT INTO person (firstName,lastName) VALUES ('"+firstName+"','"+lastName+"')";
             //avec une requete preparee
-            String sql = "INSERT INTO person (firstName,lastName) VALUES (?,?)";*/
+            String sql = "INSERT INTO person (firstName,lastName) VALUES (?,?)";
 
            /* //Facon 1 => execution de requete sans retour
             Statement statement = conn.createStatement();
@@ -31,13 +31,13 @@ public class Demo1 {
                 System.out.println("Aucune données envoyées par la requête.");
             }*/
 
-          /*  //Facon 2 => avec une requete preparee
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+           //Facon 2 => avec une requete preparee
+            /*PreparedStatement preparedStatement = conn.prepareStatement(sql);
             preparedStatement.setString(1,firstName);
             preparedStatement.setString(2,lastName);
             //preparedStatement.execute(); retour boolean
             int nbRow = preparedStatement.executeUpdate();
-            System.out.println("Nombre de ligne "+nbRow);
+            System.out.println("Nombre de ligne "+nbRow);*/
 
             //Version avec récuperation de l'Id générer
             PreparedStatement preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
@@ -48,17 +48,17 @@ public class Demo1 {
             System.out.println("Nombre de ligne "+nbRow);
             if(resultSet.next()){
                 System.out.println(resultSet.getInt(1));
-            }*/
+            }
 
             //FACON 3 => avec une requete de lecture
-            String sql = "SELECT * FROM person";
+            /*String sql = "SELECT * FROM person";
             Statement statement = conn.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()){
                 System.out.println(resultSet.getInt("id")+") "+
                                     resultSet.getString("firstName")+" "+
                                     resultSet.getString("lastName"));
-            }
+            }*/
 
         }catch (SQLException e){
             System.out.println(e.getMessage());
---
+++ Demo/src/main/java/org/example/utils/DatabaseManager.java
@@ -0,0 +1,26 @@
+package org.example.utils;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+
+    public static Connection getPostgresSQLConnection(){
+        //Test de connexion à la BDD
+        String dbUrl = "jdbc:postgresql://localhost:5432/dbDemo";
+        String userName = "postgres";
+        String userPassword = "Wazemmeslille59!!!";
+
+        try{
+            Connection conn = DriverManager.getConnection(dbUrl,userName,userPassword);
+            if(conn != null){
+                System.out.println("Connexion OK !");
+            }else{
+                System.out.println("Connexion ECHOUE !");
+            }
+            return conn;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
