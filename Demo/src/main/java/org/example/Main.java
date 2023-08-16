86a686cff8e316070a84db998c26eb3a3b64fad6
aurelie661 <boutaurel59@gmail.com>
2023-08-16 16:51:06 +0200

    exo jdbc-postgreSQL en cours

--- Demo/src/main/java/org/example/Main.java
+++ Demo/src/main/java/org/example/Main.java
@@ -7,16 +7,7 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
 
-        /*try{
-            Connection conn = DriverManager.getConnection(dbUrl,userName,userPassword);
-            if(conn != null){
-                System.out.println("Connexion OK !");
-            }else{
-                System.out.println("Connexion ECHOUE !");
-            }
-        }catch (SQLException e){
-            throw new RuntimeException(e);
-        }*/
+
 
     }
 }
